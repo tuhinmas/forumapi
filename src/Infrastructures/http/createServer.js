a77922a8bc5b869295174236b8635e4c541bf220
mastuhin <mastuhin33@gmail.com>
2023-10-18 21:40:36 +0700

    root path

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -63,6 +63,14 @@ const createServer = async (container) => {
     },
   ]);
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello world!',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const {
