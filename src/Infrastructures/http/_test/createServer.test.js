dec435d2cf8a43b04c01accefc8661de28ecb84b
mastuhin <mastuhin33@gmail.com>
2023-10-18 21:21:23 +0700

    hello there

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -41,7 +41,7 @@ describe('HTTP server', () => {
 });
 
 describe('when GET /', () => {
-  it('should return 200 and Hello there', async () => {
+  it('should return 200 and hello world', async () => {
     // Arrange
     const server = await createServer({});
     // Action
@@ -52,6 +52,6 @@ describe('when GET /', () => {
     // Assert
     const responseJson = JSON.parse(response.payload);
     expect(response.statusCode).toEqual(200);
-    expect(responseJson.value).toEqual('Hello there!');
+    expect(responseJson.value).toEqual('Hello world!');
   });
 });
